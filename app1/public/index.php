a3b885b69078ffbbaa16cc7b5c82ccc36852869c
domalee <136012609+domalee@users.noreply.github.com>
2023-07-26 16:51:11 -0700

    Update index.php

--- app1/public/index.php
+++ app1/public/index.php
@@ -5,8 +5,10 @@
     require_once('../../lib/math.php');
     $res = addRandomNumbers();
 
-    print("Here's a random sum:");
-    print($res);
+    $str = "{$res[0]} + {$res[1]} = {$res[2]}";
+
+    print("Here's a random sum: ");
+    print($str);
     print("<br>");
 ?>
 </body>
